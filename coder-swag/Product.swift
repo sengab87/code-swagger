8e69709aca2a57c9f6d7b9492bde15d4a51ff71d
Ahmed Sengab <sengab87@gmail.com>
2018-12-20 10:57:48 -0500

    last

---
+++ coder-swag/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  coder-swag
+//
+//  Created by Ahmed Sengab on 12/20/18.
+//  Copyright © 2018 Ahmed Sengab. All rights reserved.
+//
+
+import Foundation
--- coder-swag/View/ProductCell.swift
+++ coder-swag/View/ProductCell.swift
@@ -9,5 +9,7 @@
 import UIKit
 
 class ProductCell: UICollectionViewCell {
-    
+    @IBOutlet weak var productImage : UIImageView!
+    @IBOutlet weak var productTitle : UILabel!
+    @IBOutlet weak var productPrice : UILabel!
 }

96a3b31ec69ae89894ef4dbf9cc8846c2728a4be
Ahmed Sengab <sengab87@gmail.com>
2018-12-20 06:14:13 -0500

    third lesson

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Ahmed Sengab on 12/20/18.
+//  Copyright © 2018 Ahmed Sengab. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
